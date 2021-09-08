9791534f0e9dcf7338b10ae7985e0ee89b2e9486
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-08 22:08:46 +0200

    Format storybook files

--- src/stories/FormatDistance.stories.tsx
+++ src/stories/FormatDistance.stories.tsx
@@ -1,44 +1,43 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { FormatDistance } from "../react-date-fns-hooks";
 import { add } from "date-fns";
 
 export default {
-  title: 'FormatDistance',
+  title: "FormatDistance",
   addSuffix: true,
   component: FormatDistance,
 } as ComponentMeta<typeof FormatDistance>;
 
-const Template: ComponentStory<typeof FormatDistance> = (args) => <FormatDistance {...args} />;
+const Template: ComponentStory<typeof FormatDistance> = (args) => (
+  <FormatDistance {...args} />
+);
 
 const date = new Date();
 
 export const Seconds = Template.bind({});
 Seconds.args = {
-  date: add(date, {seconds: -10}),
+  date: add(date, { seconds: -10 }),
   includeSeconds: true,
-  addSuffix: true
+  addSuffix: true,
 };
 
 export const Minutes = Template.bind({});
 Minutes.args = {
-  date: add(date, {minutes: -4}),
+  date: add(date, { minutes: -4 }),
   includeSeconds: true,
-  addSuffix: true
+  addSuffix: true,
 };
 
 export const Days = Template.bind({});
 Days.args = {
-  date: add(date, {days: -4}),
-  addSuffix: true
+  date: add(date, { days: -4 }),
+  addSuffix: true,
 };
 
 export const Months = Template.bind({});
 Months.args = {
-  date: add(date, {months: -4, days: 3}),
-  addSuffix: true
+  date: add(date, { months: -4, days: 3 }),
+  addSuffix: true,
 };
-
-
-
--- src/stories/FormatRelative.stories.tsx
+++ src/stories/FormatRelative.stories.tsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { FormatRelative } from "../react-date-fns-hooks";
 import { add } from "date-fns";
 
 export default {
-  title: 'FormatRelative',
+  title: "FormatRelative",
   addSuffix: true,
   component: FormatRelative,
 } as ComponentMeta<typeof FormatRelative>;
 
-const Template: ComponentStory<typeof FormatRelative> = (args) => <FormatRelative {...args} />;
+const Template: ComponentStory<typeof FormatRelative> = (args) => (
+  <FormatRelative {...args} />
+);
 
 const date = new Date();
 
 export const Seconds = Template.bind({});
 Seconds.args = {
-  date: add(date, {seconds: -10})
+  date: add(date, { seconds: -10 }),
 };
 
 export const Minutes = Template.bind({});
 Minutes.args = {
-  date: add(date, {minutes: -4})
+  date: add(date, { minutes: -4 }),
 };
 
 export const Days = Template.bind({});
 Days.args = {
-  date: add(date, {days: -4})
+  date: add(date, { days: -4 }),
 };
 
 export const Months = Template.bind({});
 Months.args = {
-  date: add(date, {months: -4, days: 3})
+  date: add(date, { months: -4, days: 3 }),
 };
-
-
-
